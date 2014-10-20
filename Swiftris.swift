32e64c487ee198590ca1b86e744b899dab62f2d6
Benjamin Tan <benjamintanweihao@gmail.com>
2014-10-20 15:46:05 +0800

    Stopping at Chapter 9. I need a break.

--- Swiftris.swift
+++ Swiftris.swift
@@ -15,10 +15,29 @@ let StartingRow    = 0
 let PreviewColumn = 12
 let PreviewRow    = 1
 
+protocol SwiftrisDelegate {
+    // invoked when the current round of Swiftris ends
+    func gameDidEnd(swiftris: Swiftris)
+
+    // invoked immediately after a new game has begun
+    func gameDidBegin(swiftris: Swiftris)
+
+    // Invoked when the falling shape has become part of the game board
+    func gameShapeDidLand(swiftris: Swiftris)
+
+    // Invoked when the falling shaped has changed its location after being dropped
+    func gameShapeDidDrop(swiftris: Swiftris)
+
+    // Invoked hen the game has reached a new level
+    func gameDidLevelUp(swiftris: Swiftris)
+}
+
+
 class Swiftris {
     var blockArray:Array2D<Block>
     var nextShape:Shape?
     var fallingShape:Shape?
+    var delegate:SwiftrisDelegate?
 
     init() {
         fallingShape = nil
